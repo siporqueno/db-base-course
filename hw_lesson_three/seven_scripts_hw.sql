b41b274d33741c52a1096b3a6c2a1f1c6abc9ac8
siporqueno <strike1000001@gmail.com>
2020-11-04 11:17:14 +0300

    Tasks 3-7 with some amendments in 3-5

--- hw_lesson_three/seven_scripts_hw.sql
+++ hw_lesson_three/seven_scripts_hw.sql
@@ -59,6 +59,8 @@ FROM
     employees.departments d ON d_e.dept_no = d.dept_no
         JOIN
     employees.salaries s ON e.emp_no = s.emp_no
+WHERE
+    s.to_date = '9999-01-01'
 GROUP BY d.dept_name
 ORDER BY average_salary DESC;
 
@@ -74,15 +76,27 @@ GROUP BY e.emp_no
 ORDER BY max_salary DESC;
 
 -- 3. Удалить одного сотрудника, у которого максимальная зарплата.
+-- решение
 
-DELETE FROM employees.salaries 
+DELETE FROM employees.employees 
 WHERE
-    salary = (SELECT 
-        MAX(salary)
+    emp_no = (SELECT 
+        emp_no
     FROM
-        employees.salaries);
+        employees.salaries
+    ORDER BY salary DESC
+    LIMIT 1);
+    
+-- запросы для вставки модельных строк и проверки до/после действия запроса-решения
+SELECT * FROM employees.employees ORDER BY emp_no DESC;
+SELECT * FROM employees.salaries ORDER BY salary DESC;
+
+INSERT INTO employees.employees VALUES(555555, '1999-01-01', 'mock_first_nm', 'mock_last_nm', 'M', '2019-01-15');
+INSERT INTO employees.salaries VALUES(555555, 210000, '2001-03-22', '2002-03-22');
+INSERT INTO employees.salaries VALUES(555555, 220000, '2002-03-22', '9999-01-01');
 
 -- 4. Посчитать количество сотрудников во всех отделах.
+-- Подсчет кол-ва сотрудников в каждом отделе (первый вариант понимания задачи)
 
 SELECT 
     d.*, COUNT(e.emp_no) AS staff_no
@@ -94,4 +108,32 @@ FROM
     employees.departments d ON d_e.dept_no = d.dept_no
 GROUP BY d.dept_no;
 
+-- Подсчет общего кол-ва сотрудников во всех отделах (второй вариант понимания задачи)
+
+SELECT 
+    COUNT(e.emp_no) AS staff_no_in_all_departments
+FROM
+    employees.employees e
+        JOIN
+    employees.dept_emp d_e ON e.emp_no = d_e.emp_no
+        JOIN
+    employees.departments d ON d_e.dept_no = d.dept_no;
+
 -- 5. Найти количество сотрудников в отделах и посмотреть, сколько всего денег получает отдел.
+
+SELECT 
+    d.*,
+    COUNT(e.emp_no) AS staff_no,
+    SUM(salary) AS total_salary
+FROM
+    employees.salaries s
+        JOIN
+    employees.employees e ON s.emp_no = e.emp_no
+        JOIN
+    employees.dept_emp d_e ON e.emp_no = d_e.emp_no
+        JOIN
+    employees.departments d ON d_e.dept_no = d.dept_no
+WHERE
+    s.to_date = '9999-01-01'
+GROUP BY d.dept_no
+ORDER BY d.dept_no;
