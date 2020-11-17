b4afab5ad501fd98b9663884464f282e69032903
siporqueno <strike1000001@gmail.com>
2020-11-18 00:25:23 +0300

    Task 3 of interview tasks, adapted queries from Task 1 and Task 2 for new schema like_user_to_entity. Also made chart of new schema

--- hw_lesson_seven/interview_task_3.sql
+++ hw_lesson_seven/interview_task_3.sql
@@ -114,5 +114,100 @@ CREATE TABLE `like_user_to_entity`.`like_entity` (
     INSERT INTO `like_user_to_entity`.`like_entity` (`user_id`, `entity_id`) VALUES ('7', '6');
     INSERT INTO `like_user_to_entity`.`like_entity` (`user_id`, `entity_id`) VALUES ('8', '6');
 
+    -- Создайте запрос, который выведет информацию:
+-- ● id пользователя;
+-- ● имя;
+-- ● лайков получено;
+-- ● лайков поставлено;
+-- ● взаимные лайки.
     
-  
+    SELECT 
+    u.*,
+    '' AS 'likes received',
+    COUNT(*) AS `likes given`,
+    '' AS 'mutual likes'
+FROM
+    like_user_to_entity.`user` u
+        JOIN
+    like_user_to_entity.like_entity le ON u.id = le.user_id
+        JOIN
+    like_user_to_entity.entity e ON le.entity_id = e.id
+WHERE
+    u.id = 6 
+UNION SELECT 
+    u.*, COUNT(*) AS `likes received`, '', ''
+FROM
+    like_user_to_entity.`user` u
+        JOIN
+    like_user_to_entity.entity e ON u.entity_id = e.id
+        JOIN
+    like_user_to_entity.like_entity le ON e.id = le.entity_id
+WHERE
+    u.id = 6 
+UNION SELECT 
+    u.*, '', '', COUNT(*)
+FROM
+    like_user_to_entity.`user` u
+        JOIN
+    like_user_to_entity.like_entity le1 ON u.id = le1.user_id
+        JOIN
+    like_user_to_entity.like_entity le2 ON le1.entity_id = le2.entity_id
+        JOIN
+    like_user_to_entity.`user` u2 ON le2.user_id = u2.id
+WHERE
+    u.id = 6 AND u2.id = 6;
+    
+    -- Задача 2. Для структуры из задачи 1 выведите список всех пользователей, которые поставили лайк
+-- пользователям A и B (id задайте произвольно), но при этом не поставили лайк пользователю C.
+
+-- Выберем пользователей, которые лайкнули пользователей с ид 4 и 6
+SELECT 
+    u.*
+FROM
+    like_user_to_entity.`user` u
+        JOIN
+    like_user_to_entity.like_entity le ON u.id = le.user_id
+        JOIN
+    like_user_to_entity.`user` u2 ON le.entity_id = u2.entity_id
+WHERE
+    u2.id = 4 OR u2.id = 6
+GROUP BY u.id
+HAVING COUNT(*) = 2;
+
+-- Выберем пользователей, которые не лайкнули пользователя с ид 5
+    SELECT
+    le1.user_id
+FROM
+    (SELECT 
+        le.*
+    FROM
+        like_user_to_entity.like_entity le
+    JOIN like_user_to_entity.`user` u ON le.entity_id = u.entity_id
+    WHERE
+        u.id = 5) AS leu5
+        RIGHT JOIN
+    like_user_to_entity.like_entity le1 ON leu5.user_id = le1.user_id
+WHERE
+    leu5.user_id IS NULL;
+
+-- Теперь соединяем два запроса в один
+SELECT 
+    le.user_id, u2.name
+FROM
+    (SELECT 
+        le.*
+    FROM
+        like_user_to_entity.like_entity le
+    JOIN like_user_to_entity.`user` u ON le.entity_id = u.entity_id
+    WHERE
+        u.id = 5) AS leu5
+        RIGHT JOIN
+    like_user_to_entity.like_entity le ON leu5.user_id = le.user_id
+        JOIN
+    like_user_to_entity.`user` u ON le.entity_id = u.entity_id
+    JOIN
+    like_user_to_entity.`user` u2 ON le.user_id = u2.id
+WHERE
+    (leu5.user_id IS NULL AND u.id = 4)
+        OR (leu5.user_id IS NULL AND u.id = 6)
+        GROUP BY le.user_id HAVING COUNT(le.user_id)=2;
